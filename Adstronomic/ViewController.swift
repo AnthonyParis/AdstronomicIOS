e0ef23c5e4a00138c8a94db0af07c60cb27419e7
Pythony <contact@pythony.dev>
2021-12-05 13:53:14 +0100

    Initial Commit

---
+++ Adstronomic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Adstronomic
+//
+//  Created by Pythony on 05/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
